6f5032b8839b134b50bbf9d0197b6adf0c4fc3d0
vgoylo <y_draiv.by@mail.ru>
2021-06-25 17:45:09 +0300

    Fix wrong number of arguments (given 0, expected 1)

--- num_of_subarrys.rb
+++ num_of_subarrys.rb
@@ -1,21 +1,16 @@
-def num_of_subarrays(arr)
+def num_of_subarrays(arr = nil)
   result = 0
 
-  if arr.kind_of?(Array) 
+  if arr.kind_of?(Array) && arr != nil
     arr.each do |element|
       if element.kind_of?(Array)
         result += 1 
       end
-    end  
-  else
-    result = 0 
+    end   
   end
 
   result
 end
-
-
-
 
 puts num_of_subarrays([[1, 2, 3], [1, 2, 3], [1, 2, 3], 6, 7]) 
 puts num_of_subarrays(5) 
