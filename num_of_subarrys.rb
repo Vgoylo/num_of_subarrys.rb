8e3c2a4ddf9caf4d3840f0e7f80582ca4f363c9d
vgoylo <y_draiv.by@mail.ru>
2021-06-25 17:20:17 +0300

    Initiale function

--- num_of_subarrys.rb
+++ num_of_subarrys.rb
@@ -1,9 +1,14 @@
 def num_of_subarrays(arr)
   result = 0
-  arr.each do |element|
-    if element.kind_of?(Array)
-      result += 1 
-    end
+
+  if arr.kind_of?(Array) 
+    arr.each do |element|
+      if element.kind_of?(Array)
+        result += 1 
+      end
+    end  
+  else
+    result = 0 
   end
 
   result
@@ -11,8 +16,7 @@ end
 
 
 
-puts num_of_subarrays([[1, 2, 3]]) 
 
-puts num_of_subarrays([[1, 2, 3], [1, 2, 3], [1, 2, 3]]) 
-
-puts num_of_subarrays([[1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3]])
+puts num_of_subarrays([[1, 2, 3], [1, 2, 3], [1, 2, 3], 6, 7]) 
+puts num_of_subarrays(5) 
+puts num_of_subarrays
